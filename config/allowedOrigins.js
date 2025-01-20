10a34906d0c04eeab43e361341de16e18d472cbe
SHINCHANN007 <anuragkhokale006@gmail.com>
2025-01-20 23:14:32 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@
 const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://127.0.0.1:5501', 
-    'http://localhost:5500'
+    'http://localhost:5500',
+    'https://frontend-6339.onrender.com'
     // Another front-end origin
     
 ];
